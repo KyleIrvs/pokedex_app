589628ceb74831e6f69da6c6b9fe5e8bef06f3e7
Kyle Irvine <kyle.irvine98@gmail.com>
2023-08-09 13:12:56 +0200

    small ui change

--- lib/widgets/pokemon_type_stats.dart
+++ lib/widgets/pokemon_type_stats.dart
@@ -11,84 +11,80 @@ class PokemonTypeStats extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text(
-                'Type',
-                textAlign: TextAlign.start,
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              'Type',
+              textAlign: TextAlign.start,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [for (final t in pokemon.types) PokemonType(type: t)],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [for (final t in pokemon.types) PokemonType(type: t)],
+          ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              'Strengths',
+              textAlign: TextAlign.start,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
           ),
-          const SizedBox(
-            height: 12,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [for (final t in pokemon.strengths) PokemonType(type: t)],
           ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text(
-                'Strengths',
-                textAlign: TextAlign.start,
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              'Weaknesses',
+              textAlign: TextAlign.start,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                for (final t in pokemon.strengths) PokemonType(type: t)
-              ],
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              for (final t in pokemon.weaknesses) PokemonType(type: t)
+            ],
           ),
-          const SizedBox(
-            height: 12,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text(
-                'Weaknesses',
-                textAlign: TextAlign.start,
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                for (final t in pokemon.weaknesses) PokemonType(type: t)
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
