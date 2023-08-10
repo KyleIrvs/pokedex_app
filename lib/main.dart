e48984fa94ebd07e201599e85bf1ca53cfffaf40
Kyle Irvine <kyle.irvine98@gmail.com>
2023-08-10 16:22:03 +0200

    update app title

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PokeDex',
       theme: themeData,
-      home: RegionsScreen(),
+      home: const RegionsScreen(),
     );
   }
 }
